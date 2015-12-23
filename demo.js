7372bda5778b1866932d3e71d88890040f8b663d
smancke <s.mancke@tarent.de>
2015-12-23 11:12:03 +0100

    some functions on the api

--- demo.js
+++ demo.js
@@ -1,13 +1,49 @@
 
-console.log("demo");
 
-var g = new Guble("http://127.0.0.1:8080");
-g.onMessage(function(meta, header, body) {
-    $( "body" ).append("<div>"+body+"</div>");
+
+
+app = angular.module("demo", []);
+
+app.controller("demoCtrl", function($scope) {
+
+    $scope.reconnect = function() {
+        $scope.g = new Guble("http://10.86.17.26:8080");
+        $scope.g.connect("webapp");
+    }
+    
+    
+    $scope.send = function(message) {
+        $scope.g.sendMessage("/gcm/broadcast", JSON.stringify(message));
+    }
+    
+    $scope.predefinedMessages = [
+        {
+            "type": "marketing",
+            "data": {
+                "title": "Neue Produkte in ihrer Einkaufliste",
+                "message": "Es wurden neue Produkte zu ihrer Einkaufsliste hinzugefügt.",
+                "link": "einkaufsliste"
+            }
+        },
+        {
+            "type": "marketing",
+            "data": {
+                "title": "Payback update!",
+                "message": "Ihnen wurden neue Payback-Punkte gutgeschrieben!",
+                "link": "payback"
+            }
+        },
+        {
+            "type": "marketing",
+            "data": {
+                "title": "Neue REWE-Produkte",
+                "message": "Schauen Sie doch mal in ihre Angebote - es gibt neue leckere Schmeckerlis.",
+                "link": "angebote"
+            }
+        }        
+    ];
+    $scope.message = $scope.predefinedMessages[0];
 });
-g.onOpen(function() {
-    g.sendRaw("+ /foo");
-    g.sendRaw("> /foo\n\nHallo, das ist eine Demo");
-});
-g.connect("marvin");
+
+
 
